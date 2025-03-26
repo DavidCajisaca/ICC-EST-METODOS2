833de141a8f0a71c58455bbb5e61cdf01585303d
Dennis Cajisaca <cajisacadennis@gmail.com>
2025-03-25 19:21:50 -0500

    Hola

--- src/App.java
+++ src/App.java
@@ -17,28 +17,19 @@ public class App {
         System.out.println("\n"+"Arreglo ordenado descendentemente");
         s.Seleccionascendente(arre2);
         s.printarreglo(arre2);
-        //s.printarreglo(arre2);
-       // s.Seleccionascendente(arre2);
-        //s.Seleccionascendente(arre);
-        //s.printarreglo(arre);
-       // s.ordenardescendente(arre2);
-       // s.printarreglo(arre2);
-        //imprimir dependiendo des
-        // s.ordenardesle(multi, true);
-        // s.printarreglo(multi);
-        // System.out.println("con false ");
-        // s.ordenardesle(multi, false);
-        // s.printarreglo(multi);
+      
 
         //metodo de inserccion
         System.out.println("\n");
         System.out.println("----Metodo inserccion----"); 
+
         Inserccion i = new Inserccion();
+        System.out.println("Arreglo original");
+
         i.printarreglo(arre2);
         i.ordenascendente(arre2, true);
         i.printarreglo(arre2);
         System.out.println("\n"+"inserccion sin pasos"); 
-      
         i.ordenascendente(arre2, false);
         i.printarreglo(arre2);
 
